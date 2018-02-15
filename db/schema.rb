6a16de26b7fdb4a295638919b3c483ecf4c8b51e
bgood92 <goodfellow.brandon@gmail.com>
2018-02-15 11:32:05 -0600

    Created a scaffold for Type model and migrated to the database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180215172827) do
+ActiveRecord::Schema.define(version: 20180215173013) do
 
   create_table "pokemons", force: :cascade do |t|
     t.integer "pokedex"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 20180215172827) do
     t.integer "sp_atk"
     t.integer "sp_def"
     t.integer "speed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "types", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
